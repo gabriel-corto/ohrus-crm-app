7923e666430b3233be8652b29c00c58ce3dc4b78
Gabriel Francisco <105436166+gabriel-corto@users.noreply.github.com>
2025-11-26 07:44:45 +0100

    Add files via upload

---
+++ costumers.ts
@@ -0,0 +1,15 @@
+const costumers = [
+  { id: 1, name: "João Silva", email: "joao@gmail.com", phone: "912345678" },
+  {
+    id: 2,
+    name: "Maria Costa",
+    email: "maria@gmail.com",
+    phone: "923456789",
+  },
+  {
+    id: 3,
+    name: "Ana Santos",
+    email: "ana@example.com",
+    phone: "934567890",
+  },
+];
